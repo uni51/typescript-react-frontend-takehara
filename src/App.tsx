efca8f50f518172e13c2310ec6473fe5190308a7
uni51 <uni51@users.noreply.github.com>
2020-07-17 21:48:11 +0900

    05.Intersection Types & 06.Union Types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { type } from "os";
 
 const name = "hello";
 
@@ -24,6 +25,35 @@ let nameObj: NAME = { first: "Yamada", last: null };
 const func1 = (x: number, y: number): number => {
   return x + y;
 };
+
+// Intersection Types（指定された型をすべて満たす型）
+type PROFILE = {
+  age: number;
+  city: string;
+};
+
+type LOGIN = {
+  username: string;
+  password: string;
+};
+
+type USER = PROFILE & LOGIN;
+
+const userA: USER = {
+  age: 30,
+  city: "Tokyo",
+  username: "xxx",
+  password: "yyyy",
+};
+
+// Union Types
+let value: boolean | number;
+value = true;
+value = 10;
+
+// 配列の要素の型を指定
+let arrayUni: (number | string)[];
+arrayUni = [0, 1, 2, "hello"];
 
 function App() {
   return (
