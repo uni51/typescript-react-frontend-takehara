b61ad028f4c099f5d4be112b9cdbf1b0bbc4ad2d
uni51 <uni51@users.noreply.github.com>
2020-08-03 10:15:12 +0900

    13. JSON型推論

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Data from "./data.json";
 import { type } from "os";
+
+type USERS = typeof Data;
 
 const name = "hello";
 
