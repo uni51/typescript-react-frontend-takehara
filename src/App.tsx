ef253ac4a20ee7c2b053940a4b134e45648da864
uni51 <uni51@users.noreply.github.com>
2020-08-03 10:09:14 +0900

    12. Generics

--- src/App.tsx
+++ src/App.tsx
@@ -122,6 +122,51 @@ let funcComp2 = (x: string) => {};
 // funcComp1 = funcComp2; // エラーになる
 // funcComp2 = funcComp1; // エラーになる
 
+// Generics ジェネリクス
+interface GEN<T> {
+  item: T;
+}
+const gen0: GEN<string> = { item: "hello" };
+// const gen1: GEN = { item: "hello" }; // エラーになる
+const gen2: GEN<number> = { item: 12 };
+
+// デフォルトの型を指定
+interface GEN1<T = string> {
+  item: T;
+}
+const gen3: GEN1 = { item: "hello" };
+
+interface GEN2<T extends string | number> {
+  item: T;
+}
+const gen4: GEN2<string> = { item: "hello" };
+// const gen4: GEN2<boolean> = { item: true }; // エラーになる
+
+function funcGen<T>(props: T) {
+  return { item: props };
+}
+const gen6 = funcGen<string>("test");
+// const gen6 = funcGen("test"); // 関数の場合は、明示的に指定しなくても型推論をしてくれる
+const gen7 = funcGen<string | null>(null);
+
+function funcGen1<T extends string | null>(props: T) {
+  return { value: props };
+}
+const gen8 = funcGen1("hello");
+// const gen9 = funcGen1(123); // エラーになる
+
+interface Props {
+  price: number;
+}
+function funcGen3<T extends Props>(props: T) {
+  return { value: props.price };
+}
+const gen10 = funcGen3({ price: 123 });
+
+const funcGen4 = <T extends Props>(props: T) => {
+  return { value: props.price };
+};
+
 function App() {
   return (
     <div className="App">
